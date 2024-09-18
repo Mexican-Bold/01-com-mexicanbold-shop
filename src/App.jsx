e96ef95c0d8dfa681cec4f2510e424446d6be427
Chef <cheftech@patraldo.com>
2024-09-17 18:00:06 -0600

    personalization

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,18 @@ function App() {
     <div class={styles.App}>
       <header class={styles.header}>
         <img src={logo} class={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <a
+                <a
           class={styles.link}
           href="https://github.com/solidjs/solid"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Solid
+         Mexican Bold - Orgullosamente Chingón  
         </a>
+	  <p>
+          Estilo de Rock Stars para tí y para mí.
+        </p>
+
       </header>
     </div>
   );
